adeab58dd15ae3a02348445daeaed86888bddaea
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-03-16 23:34:19 +0600

    Creat components for navbar

---
+++ tailwind-react-pricing-options/src/components/DaisyNav/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+          <ul className='flex'>
+            <li class='mr-10'><a href=''>Home</a></li>
+            <li class='mr-10'><a href=''>About</a></li>
+            <li class='mr-10'><a href=''>Blog</a></li>
+            </ul>  
+        </nav>
+    );
+};
+
+export default Navbar;
